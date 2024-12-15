3d22f45912f58203c9d6a9a287be50f91d650e5f
Moomoocoder <167264789+Moomoocoder@users.noreply.github.com>
2024-12-15 10:49:11 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,14 +2,6 @@ function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
 
-function acceptTerms() {
-for (var i = 5; i > 3; i = i + 1)
-  { console.log(i); }
-};
-
-function declineTerms() {
-    console.log("Declined terms!");
-}
 
 document.addEventListener('keydown', function(event) {
     if (event.key === "Escape") {
